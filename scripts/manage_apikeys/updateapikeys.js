9dad79ea679730f61fde64cea79e3eba62a4fbbc
adodge-bcg <alan.dodge@gov.bc.ca>
2021-09-17 07:46:34 -0700

    Refactor connection info into a var to remove code duplication

--- scripts/manage_apikeys/updateapikeys.js
+++ scripts/manage_apikeys/updateapikeys.js
@@ -2,22 +2,18 @@ const { Pool } = require('pg');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
-const pool = new Pool({
+const connectInfo = {
     user: process.env.DB_USER ? process.env.DB_USER : 'ckan',
     host: process.env.DB_HOST ? process.env.DB_HOST : '127.0.0.1',
     database: process.env.DB_NAME ? process.env.DB_NAME : 'ckan',
     password: process.env.DB_PASS ? process.env.DB_PASS : 'ckan',
     port: process.env.DB_PORT ? process.env.DB_PORT : 5432,
-  });
-  pool.connect();
+};
+
+const pool = new Pool(connectInfo);
+pool.connect();
   
-  console.log({
-      user: process.env.DB_USER ? process.env.DB_USER : 'ckan',
-      host: process.env.DB_HOST ? process.env.DB_HOST : '127.0.0.1',
-      database: process.env.DB_NAME ? process.env.DB_NAME : 'ckan',
-      password: process.env.DB_PASS ? process.env.DB_PASS : 'ckan',
-      port: process.env.DB_PORT ? process.env.DB_PORT : 5432,
-  });
+console.log(connectInfo);
 
 async function getUserNames() {
     try {
